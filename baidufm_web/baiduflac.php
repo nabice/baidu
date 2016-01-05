d1a204b31c2823625687c1cd1043205ce96abc62
nabice <nabice@gmail.com>
2016-01-05 11:31:32 +0800

    update

--- baidufm_web/baiduflac.php
+++ baidufm_web/baiduflac.php
@@ -2,7 +2,11 @@
 $id = intval($_GET['id']);
 $musicurl = "http://music.baidu.com/data/music/fmlink?songIds=".$id;
 $songinfo = json_decode(file_get_contents($musicurl), true);
-exec("wget -O /tmp/baidufm.lrc http://music.baidu.com".escapeshellarg($songinfo['data']['songList'][0]['lrcLink']));
+if($songinfo['data']['songList'][0]['lrcLink']){
+    exec("wget -O /tmp/baidufm.lrc http://music.baidu.com".escapeshellarg($songinfo['data']['songList'][0]['lrcLink']));
+}else{
+    unlink("/tmp/baidufm.lrc");
+}
 $flacurl = exec("/home/nabice/src/baiduflac ".$id." -u");
 pclose(popen("/bin/ps -ef|/bin/grep baidulrc| /bin/grep -v grep|awk '{print $2}'|xargs -I % super kill %;DISPLAY=:0 /home/nabice/src/baidulrc &", "r"));
 if($flacurl){
